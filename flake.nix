18589ef925257419f66c6879a5e53cb622cf7324
Gako358 <gako358@outlook.com>
2023-04-19 08:18:20 +0200

    setting up flake

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , naersk
+    ,
+    }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      naersk-lib = pkgs.callPackage naersk { };
+      # Package set for this system, add packages here
+    in
+    {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          cargo
+          rustc
+          rustfmt
+          pre-commit
+          rustPackages.clippy
+          pkg-config
+        ];
+      };
+    });
+}
